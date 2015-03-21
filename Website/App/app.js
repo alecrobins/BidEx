bfe707d642d99aeb047718558dab33304dc070d7
Alec Robins <alecrobins@gmail.com>
2015-03-21 16:54:19 +0800

    Item page framework set

--- Website/App/app.js
+++ Website/App/app.js
@@ -1,14 +1,15 @@
 'use strict';
 //TODO: function that
-var login = angular.module('LoginCheck', []).
-factory('$logincheck', function () {
-    return function () {
-        return false;
-    };
-});
+// var login = angular.module('LoginCheck', []).
+// factory('$logincheck', function () {
+//     return function () {
+//         return false;
+//     };
+// });
 
 // Create the app
-var app = angular.module('app', ['LoginCheck', 'ngRoute', 'ngAnimate']);
+// var app = angular.module('app', ['LoginCheck', 'ngRoute', 'ngAnimate']);
+var app = angular.module('app', ['ngRoute', 'ngAnimate']);
 
 // Configure the routes
 app.config(['$routeProvider', function ($routeProvider) {
@@ -33,37 +34,42 @@ app.config(['$routeProvider', function ($routeProvider) {
         .when('/item', {
             templateUrl: 'Templates/item.html',
             controller: 'ItemController',
+            resolve: {
+              getItem: itemController.getItem
+            }
         })
 
-				// route for the specific item page page
-				// TODO: need to route the specific item (deal) id with this page
-        .when('/item/:id', {
-            templateUrl: 'Templates/item.html',
-            controller: 'ItemController',
-        })
-
-				// The completed page for an item
-        .when('/item/:id/complete', {
-            templateUrl: 'Templates/item.html',
-            controller: 'ItemController',
-        })
+				// // route for the specific item page page
+				// // TODO: need to route the specific item (deal) id with this page
+        // .when('/item/:id', {
+        //     templateUrl: 'Templates/item.html',
+        //     controller: 'ItemController',
+        // })
+        //
+				// // The completed page for an item
+        // .when('/item/:id/complete', {
+        //     templateUrl: 'Templates/item.html',
+        //     controller: 'ItemController',
+        // })
 
         .otherwise({
             redirectTo: '/login',
         });
 
 
-}]).
-  run(function ($rootScope, $logincheck, $location) {
-      $rootScope.$on("$locationChangeStart", function (event, next, current) {
-        //on any route change if user is not logged in then go to log in
-        console.log("Starting the check for login . . . ");
+}]);
 
-				//TODO: check for user log in . . .
-
-				// if (!$logincheck()) {
-        //     console.log("NOT LOGGED IN");
-        //     $location.path('/login');
-        // }
-    });
-});
+//TODO: add functionality on user log in
+// app.run(function ($rootScope, $logincheck, $location) {
+//       $rootScope.$on("$locationChangeStart", function (event, next, current) {
+//         //on any route change if user is not logged in then go to log in
+//         console.log("Starting the check for login . . . ");
+//
+// 				//TODO: check for user log in . . .
+//
+// 				// if (!$logincheck()) {
+//         //     console.log("NOT LOGGED IN");
+//         //     $location.path('/login');
+//         // }
+//     });
+// });
--- Website/App/controllers/ItemController.js
+++ Website/App/controllers/ItemController.js
@@ -1,8 +1,75 @@
 'use strict';
 
-var itemController = angular.module("app").controller('ItemController', ['$scope',
-function ($scope) {
+var itemController = angular.module("app").controller('ItemController', ['$scope', '$route',
+function ($scope, $route) {
 
 		$scope.test = "Hello World - item";
 
+		$scope.item = $route.current.locals.getItem;
+
 }]);
+
+itemController.getItem = function ($q, $timeout) {
+  var deferred = $q.defer();
+
+	var item = {
+		"title": "One Hour Conversation with Mark Zuckerburg",
+		"featureImage": "Assets/Images/filler.png",
+		"currentBid": 2300,
+		"endTime": "", // TODO: need to fill with a datetime
+		"remainingTime": "0:59", // TODO: NEED TO REMOVE,
+		"bidInc": 100,
+		"topBidder": {
+			"username": "alecrobins",
+			"profile": "Assets/Images/pro1.png"
+		},
+		"comments": [
+			{
+				"user": {
+					"username": "exampleUser",
+					"bidAmount": 2200,
+					"profile": "Assets/Images/pro1.png"
+				},
+				"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacus lacus, viverra in pellentesque et, faucibu."
+			},
+			{
+				"user": {
+					"username": "userNumbe2",
+					"bidAmount": "",
+					"profile": "Assets/Images/pro2.png"
+				},
+				"text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut lacus lacus, viverra in pellentesque et, faucibu."
+			}
+		],
+		"bid-history": [
+			{
+				"user": {
+					"username": "exampleUser",
+					"profile": "Assets/Images/pro1.png"
+				},
+				"bidAmount": 2200
+			},
+			{
+				"user": {
+					"username": "userNumbe2",
+					"profile": "Assets/Images/pro2.png"
+				},
+				"bidAmount": 2100
+			}
+		]
+	};
+
+	deferred.resolve(item);
+
+
+  // //TODO: before calling the game service need to check the cache
+  // // to see if data has already been loaded
+  // gameService.getUpcomingGames().then(
+  //   function() {
+  //       //get upcoming games
+  //       var upcomingGames = gameService.upcomingGames();
+  //       deferred.resolve( upcomingGames );
+  //   });
+
+    return deferred.promise;
+}
